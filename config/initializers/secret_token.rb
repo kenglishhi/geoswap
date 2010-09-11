9f837d7fd77a028f699e71bf182ed25a204dddcc
Kevin English <kenglish_hi@yahoo.com>
2010-09-11 10:44:47 -1000

    New GeoSwap DB

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geoswap::Application.config.secret_token = '21dabf0e148dcb0a8501ea37771b13a46e377f8a23b00f65ad79b1fda22a838c1bcc098c414ffc3302b6df958b9199e1ec14bd719c5ed45e802ac7a23834b6e7'
